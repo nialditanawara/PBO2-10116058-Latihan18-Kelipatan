b74f6ebb851779377a3a02b2766525b73a71a2b2
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:37:04 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan18-Kelipatan/src/pbo2/pkg10116058/latihan18/kelipatan/PBO210116058Latihan18Kelipatan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan18.kelipatan;
+
+public class PBO210116058Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       for (double i=3.5; i <=35; i= i+ 3.5){
+           System.out.println(i);   
+       }
+        
+    }
+    
+}
